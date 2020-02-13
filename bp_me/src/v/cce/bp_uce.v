c3a89cf9711136ef6cfbdc53573442c3f9754b14
Dan Petrisko <petrisko@cs.washington.edu>
2020-02-12 22:18:58 -0800

    TEMP

--- bp_me/src/v/cce/bp_uce.v
+++ bp_me/src/v/cce/bp_uce.v
@@ -25,17 +25,32 @@ module bp_uce
     , output [tag_mem_pkt_width_lp-1:0] tag_mem_pkt_o
     , output [data_mem_pkt_width_lp-1:0] data_mem_pkt_o
     , output [stat_mem_pkt_width_lp-1:0] stat_mem_pkt_o
-    , output cache_fill_v_o
+    , output logic cache_fill_v_o
     , input cache_fill_yumi_i
 
     , output [cce_mem_msg_width_lp-1:0] mem_cmd_o
-    , output mem_cmd_v_o
+    , output logic mem_cmd_v_o
     , input mem_cmd_ready_i
 
     , input [cce_mem_msg_width_lp-1:0] mem_resp_i
     , input mem_resp_v_i
-    , output mem_resp_yumi_o
+    , output logic mem_resp_yumi_o
     );
+
+  `declare_bp_me_if(paddr_width_p, cce_block_width_p, lce_id_width_p, lce_assoc_p);
+  bp_cce_mem_msg_s mem_cmd_cast_o, mem_resp_cast_i;
+  assign mem_cmd_o = mem_cmd_cast_o;
+  assign mem_resp_cast_i = mem_resp_i;
+
+  `declare_bp_be_dcache_lce_stat_mem_pkt_s(lce_sets_p, lce_assoc_p);
+  `declare_bp_be_dcache_lce_tag_mem_pkt_s(lce_sets_p, lce_assoc_p, ptag_width_lp);
+  `declare_bp_be_dcache_lce_data_mem_pkt_s(lce_sets_p, lce_assoc_p, dword_width_p*lce_assoc_p);
+  bp_be_dcache_lce_stat_mem_pkt_s stat_mem_pkt_cast_o;
+  bp_be_dcache_lce_tag_mem_pkt_s tag_mem_pkt_cast_o;
+  bp_be_dcache_lce_data_mem_pkt_s data_mem_pkt_cast_o;
+  assign stat_mem_pkt_o = stat_mem_pkt_cast_o;
+  assign tag_mem_pkt_o = tag_mem_pkt_cast_o;
+  assign data_mem_pkt_o = data_mem_pkt_cast_o;
 
   enum logic [1:0] {e_ready, e_writeback, e_write_request, e_read_request} state_n, state_r;
   wire is_ready         = (state_r == e_ready);
@@ -44,43 +59,67 @@ module bp_uce
   wire is_read_request  = (state_r == e_read_request);
 
   `declare_bp_cache_service_s(paddr_width_p);
-  bp_cache_service_s cache_service_li;
-  logic cache_service_v_li, cache_service_yumi_lo;
-  bsg_one_fifo
-   #(.width_p($bits(bp_cache_service_s)))
-   service_fifo
-    (.clk_i(clk_i)
-     ,.reset_i(reset_i)
+  bp_cache_service_s cache_service_cast_i;
+  assign cache_service_cast_i = cache_service_i;
 
-     ,.data_i(cache_service_i)
-     ,.v_i(cache_service_v_i)
-     ,.ready_o(cache_service_ready_o)
+  wire miss_load_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_miss_load};
+  wire miss_store_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_miss_store};
+  wire miss_li = cache_service_v_li & (miss_load_li | miss_store_li);
+  wire uc_store_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_uc_store};
+  wire wt_store_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_wt_store};
+  wire uc_load_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_uc_load};
+  wire block_read_li = cache_service_v_li & cache_service_cast_i.msg_type inside {e_block_read};
 
-     ,.data_o(cache_service_li)
-     ,.v_o(cache_service_v_li)
-     ,.yumi_i(cache_service_yumi_lo)
-     );
-
-  wire miss_li = cache_service_v_li & cache_service_li.msg_type inside {e_miss_load, e_miss_store};
-  wire uc_store_li = cache_service_v_li & cache_service_li.msg_type inside {e_uc_store};
-  wire wt_store_li = cache_service_v_li & cache_service_li.msg_type inside {e_wt_store};
-  wire uc_load_li = cache_service_v_li & cache_service_li.msg_type inside {e_uc_load};
-  wire block_read_li = cache_service_v_li & cache_service_li.msg_type inside {e_block_read};
+  always_comb
+    unique case (state_r)
+      e_ready        : state_n = miss_li
+                                 ? cache_service_cast_i.dirty 
+                                   ? e_writeback 
+                                   : e_read_request 
+                                 : e_ready;
+      e_writeback    : state_n = block_read_li ? e_write_request : e_writeback;
+      e_write_request: state_n = mem_resp_yumi_o ? e_read_request : e_ready;
+      e_read_request : state_n = cache_service_yumi_lo ? e_ready : e_read_request;
+      default: state_n = e_ready;
+    endcase
 
   always_comb
     begin
+      stat_mem_pkt_cast_o = '0;
+      tag_mem_pkt_cast_o = '0;
+      data_mem_pkt_cast_o = '0;
+      cache_fill_v_o = '0;
+
+      mem_cmd_cast_o = '0;
+      mem_cmd_v_o = '0;
+
+      mem_resp_yumi_o = '0;
+
+      state_n = state_r;
+
+      cache_service_ready_o = '0;
+
       unique case (state_r)
-        e_ready        : state_n = miss_li
-                                   ? cache_service_li.dirty 
-                                     ? e_writeback 
-                                     : e_read_request 
-                                   : e_ready;
-        e_writeback    : state_n = block_read_li ? e_write_request : e_writeback;
-        e_write_request: state_n = mem_resp_yumi_o ? e_read_request : e_ready;
-        e_read_request : state_n = cache_service_yumi_lo ? e_ready : e_read_request;
-        default: state_n = e_ready;
-      endcase
+        e_ready: 
+          if (miss_li & ~cache_service_cast_i.dirty)
+            begin
+              mem_cmd_v_o = mem_cmd_ready_o;
+              mem_cmd_cast_o.msg_type = miss_load_li ? e_cce_mem_rd : e_cce_mem_wr;
+              mem_cmd_cast_o.addr = cache_service_cast_i.addr;
+              mem_cmd_cast_o.size = e_mem_size_64;
+              mem_cmd_cast_o.way_id = cache_service_cast_i.repl_way;
+
+              state_n = mem_cmd_v_o ? e_read_request : e_ready;
+            end
+          else if (miss_li & cache_service_cast_i.dirty)
+            begin
+              // TODO:
+              state_n = block_read_li ? e_write_request : e_writeback;
+            end
+
+
     end
+
 
   always_ff @(posedge clk_i)
     if (reset_i)
